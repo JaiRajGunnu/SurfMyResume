9b119cd4398d7875421c37cafdea2f94a4a74216
JaiRajGunnu <jairajgsklm@gmail.com>
2024-12-28 19:40:10 +0530

    ui-interface

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -66,31 +66,41 @@ const SplashScreen = ({ onStart }) => {
     <div className="splash-screen" onDoubleClick={handleDoubleClick}>
       <h1 className="tit1">LET'S SURF</h1>
       <p className="tit2">ENDLESS</p>
-      {/* <div className="highest-distance-display">
-        Highest Distance: {highestDistance} m
-      </div> */}
 
-<div className="distance-display">
-  {/* Star icon */}
-  <span id="score-icon" className="icon-container">
-    <svg className="icon-star" width="20px" height="20px" viewBox="0 0 20 20">
-      <path className="icon-fill" d="M9.10433 2.89874C9.47114 2.15549 10.531 2.1555 10.8978 
-    2.89874L12.8282 6.81024L17.1448 7.43748C17.9651 7.55666 18.2926 8.56464 17.699 
-    9.14317L14.5755 12.1878L15.3129 16.487C15.453 17.3039 14.5956 17.9269 13.8619 
-    17.5412L10.0011 15.5114L6.14018 17.5412C5.40655 17.9269 4.54913 17.3039 
-    4.68924 16.487L5.4266 12.1878L2.30308 9.14317C1.70956 8.56463
-     2.03708 7.55666 2.8573 7.43748L7.17389 6.81024L9.10433
-      2.89874Z"> </path>
-    </svg>
-  </span>
+      <div className="distance-display">
+        {/* Star icon */}
+        <span id="score-icon" className="icon-container">
+          <svg className="icon-star" width="20px" height="20px" viewBox="0 0 20 20">
+            <path className="icon-fill" d="M9.10433 2.89874C9.47114 2.15549 10.531 2.1555 10.8978 
+              2.89874L12.8282 6.81024L17.1448 7.43748C17.9651 7.55666 18.2926 8.56464 17.699 
+              9.14317L14.5755 12.1878L15.3129 16.487C15.453 17.3039 14.5956 17.9269 13.8619 
+              17.5412L10.0011 15.5114L6.14018 17.5412C5.40655 17.9269 4.54913 17.3039 
+              4.68924 16.487L5.4266 12.1878L2.30308 9.14317C1.70956 8.56463
+              2.03708 7.55666 2.8573 7.43748L7.17389 6.81024L9.10433
+              2.89874Z"> </path>
+          </svg>
+        </span>
 
-  {/* Highest distance */}
-  <span className="distance-text"> {highestDistance} m </span>
-</div>
+        {/* Highest distance */}
+        <span className="distance-text"> {highestDistance} m </span>
+      </div>
 
       <div className="character-selection">
         <button className="arrow left" onClick={handlePrev}>
-          &lt;
+          <span id="selector-left">
+            <svg
+              className="icon-leftArrow"
+              width="44px"
+              height="44px"
+              viewBox="0 0 20 20"
+              style={{ margin: "0px", display: "inline-block" }}
+            >
+              <path
+                className="icon-fill"
+                d="M12.2676 15.793C11.9677 16.0787 11.493 16.0672 11.2073 15.7672L6.20597 10.5168C5.93004 10.2271 5.93004 9.77187 6.20597 9.4822L11.2073 4.23173C11.493 3.93181 11.9677 3.92028 12.2676 4.20597C12.5676 4.49166 12.5791 4.96639 12.2934 5.26631L7.78483 9.99949L12.2934 14.7327C12.5791 15.0326 12.5676 15.5073 12.2676 15.793Z"
+              ></path>
+            </svg>
+          </span>
         </button>
         <img
           src={surfers[currentSurfer]}
@@ -98,10 +108,23 @@ const SplashScreen = ({ onStart }) => {
           className="surfer-avatar"
         />
         <button className="arrow right" onClick={handleNext}>
-          &gt;
+          <span id="selector-right">
+            <svg
+              className="icon-rightArrow"
+              width="44px"
+              height="44px"
+              viewBox="0 0 20 20"
+              style={{ margin: "0px", display: "inline-block" }}
+            >
+              <path
+                className="icon-fill"
+                d="M7.73271 4.20694C8.03263 3.92125 8.50737 3.93279 8.79306 4.23271L13.7944 9.48318C14.0703 9.77285 14.0703 10.2281 13.7944 10.5178L8.79306 15.7682C8.50737 16.0681 8.03263 16.0797 7.73271 15.794C7.43279 15.5083 7.42125 15.0336 7.70694 14.7336L12.2155 10.0005L7.70694 5.26729C7.42125 4.96737 7.43279 4.49264 7.73271 4.20694Z"
+              ></path>
+            </svg>
+          </span>
         </button>
       </div>
-      <span className="start-button">
+      <span className="start-txt">
         <span className="st-btn">{buttonText}</span> to start playing
       </span>
     </div>
