ee3e8005a285858b57246f3bf45363dff59fe36e
JaiRajGunnu <jairajgsklm@gmail.com>
2025-01-27 16:51:40 +0530

    fullscreen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import SplashScreen from "./components/SplashScreen";
 import GameContainer from "./components/GameContainer";
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./styles/App.css";
 
 const App = () => {
@@ -9,16 +9,35 @@ const App = () => {
   const [selectedSurfer, setSelectedSurfer] = useState(null);
 
   const handleStartGame = (surfer) => {
+    if (!isFullscreen()) {
+      alert("Start playing this mini-game by switching to fullscreen for a better experience.");
+      return;
+    }
     setSelectedSurfer(surfer);
     setIsGameStarted(true);
+  };
+
+  const isFullscreen = () => {
+    return document.fullscreenElement || document.webkitFullscreenElement || document.msFullscreenElement;
+  };
+
+  const requestFullscreen = () => {
+    const element = document.documentElement;
+    if (element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if (element.webkitRequestFullscreen) {
+      element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) {
+      element.msRequestFullscreen();
+    }
   };
 
   useEffect(() => {
     const handleKeyDown = (event) => {
       if (event.ctrlKey && (event.key === '+' || event.key === '=' || event.key === '-')) {
         event.preventDefault();
-         document.body.style.zoom = "100%";
-         document.documentElement.style.zoom = "100%";
+        document.body.style.zoom = "100%";
+        document.documentElement.style.zoom = "100%";
       }
     };
 
@@ -29,20 +48,17 @@ const App = () => {
     };
   }, []);
 
-    // Added HTML tags to prevent default zoom functionality
   return (
-        <body onWheel={(e) => { if (e.ctrlKey) e.preventDefault() }} onKeyDown={(e) => { if (e.ctrlKey && e.key === 'v') e.preventDefault()}}>
-             <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"></meta>
-       <div>
-      {!isGameStarted ? (
-        <SplashScreen onStart={handleStartGame} />
-      ) : (
-        <GameContainer selectedSurfer={selectedSurfer} />
-      )}
-
-
-       <SpeedInsights />
-    </div>
+    <body onWheel={(e) => { if (e.ctrlKey) e.preventDefault() }} onKeyDown={(e) => { if (e.ctrlKey && e.key === 'v') e.preventDefault()}}>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"></meta>
+      <div>
+        {!isGameStarted ? (
+          <SplashScreen onStart={handleStartGame} />
+        ) : (
+          <GameContainer selectedSurfer={selectedSurfer} />
+        )}
+        <SpeedInsights />
+      </div>
     </body>
   );
 };
